56b7dbc7e230418da9e9eca88c3130923401b300
Pablo Guarneros <wrno.pablo@gmail.com>
2021-07-22 13:30:05 -0400

    added the first poem

---
+++ rain.py
@@ -0,0 +1,35 @@
+class Drop:
+    def __init__(self, width):
+        self.height = 50
+        self.width = width
+        self.isGone = False
+
+    def down(self):
+        self.height -= 1
+        self.width -= 0.02
+        if self.width < 0:
+            self.isGone = True
+'''
+
+class Sky:
+    def __init__(self, temp):
+        self.temp = temp
+        self.drops = []
+
+    def addDrop(self, drop):
+        self.drops.append(drop)
+'''
+
+dropA = Drop(.001)
+dropB = Drop(1)
+dropC = Drop(34)
+sky = [dropA, dropB, dropC]
+
+def rain(sky):
+    for drop in sky:
+        drop.down()
+        if drop.isGone:
+            return("no splash")
+
+
+print(rain(sky))
