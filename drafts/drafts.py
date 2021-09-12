0a77569e6cd8b1172a3457ee80b1c88f54174b00
Pablo Guarneros <wrno.pablo@gmail.com>
2021-09-12 22:06:36 +0200

    Recent Berlin updates. Refactored to increase the detail of functions about water. Used more classes to work with objects rather than keep using strings. :

--- drafts/drafts.py
+++ drafts/drafts.py
@@ -39,3 +39,109 @@ while dog.is_on_walk:
 def mate_or_cannibal(this_bee, other_bee):
     while other_bee.is_dead:
         this_bee.pokes(other_bee.head)
+
+def new_beginning():
+    if tree.is_cut:
+        tree.trunk.awaits(Life())
+
+def remapping():
+    building = Mountain()
+    sidewalk_scaffolding = Cave()
+
+def difficult_thought():
+    while pandemic:
+        wild_animals.prosper()
+
+def watch_your_step(scene, street):
+    for drunk in street["rowdy_people"]:
+        scene.add("broken_glass_bottle")
+
+while city.reconstruction:
+    if need_for_apartments == "HIGH":
+        building_similarity = 0.9
+
+def pee_or_poo(dropping):
+    if dropping.has_color("all_white"):
+        dropping.is_pee = True
+
+def water_storage(storm, plant_leaves):
+    if storm.has_passed():
+        plant_leaves.add(storm.last_drops)
+
+def abandoned_corners(banister, spider_web):
+    while banister.untouched:
+        spider_web.add_weave()
+
+for shadow in street_lamp:
+    one_cat.meows()
+    one_car.stops()
+
+def test_rain(weather):
+    r = Rain()
+    weather.assertTrue(r.contains_drops)
+
+def rain_clouds_form(person):
+    if "umbrella" not in person.belongings:
+        person["searches_for"] = "cover"
+
+def texturize_surface(rain, storm, pond):
+    if rain and storm:
+        pond.create("lopsided stalagmites")
+
+for underpass in canal.hidden_corners:
+    print("untold dreams")
+    print("a life still lived")
+
+def runoff(waste):
+    if waste.exit["touches"] == "river":
+        return Oil(color="rainbow", style="concentric_circles")
+
+for flower in stall:
+    if flower not in chosen:
+        flower.is_thrown_out = True
+
+for leaf in grass:
+    h = Human()
+    life.append(h)
+
+if beach.has_people:
+    penguin.can_return = False
+    penguin.baby.can_eat = False
+
+def alert_food_availability(cheetah, scene):
+    if cheetah.calls and "human" not in scene:
+        cheetah.cubs.can_eat = True
+
+
+if "motorbike" in leaf.path() and (
+    wind.direction == lead.direction):
+    leaf.reach_destination = False
+
+for trash in bin.items:
+    if trash.has_food:
+        invite(bugs)
+    
+def cloning_sheep(street, sheep):
+    for poster in street["ads about goat cheese"]:
+        sheep.create_copy()
+
+def hangout(tree):
+    while "unflammable_rocks" in tree.get_surrounding():
+        tree.collect(Cigarette())
+
+def soothing_sleep(street_cars):
+    waves = [Wind() for car in street_cars]
+    waves.play_sound()
+
+if "practicality" in city.worldview:
+    bench["sits_couples"] = True
+    bench["is_shelter"] = False
+
+def get_probability_child_trips():
+    if tree_root.outgrows_path():
+        return 0.98
+
+def compression(wind, dandelion):
+    if wind.blowing_ferocity > dandelion.action_potential:
+        return Seed(bit_weight=dandelion.bit_weight*0.02)
+
--- final/city_cracks.py
+++ final/city_cracks.py
@@ -1,32 +1,40 @@
 ##current poem count: 40
 
-def night_drive(bus):
-    if bus.speeds({"at":"night"}):
-        corn.drifts(by = 2) #in inches
-
-def start_crisis(world):
-    while world.cities.get_built():
-        world.skies.dissapear()
-
 def wake_up(city):
-    mist.dissapears()
-    one_duck.quaks()
+    mist.disappears()
+    one_duck.quacks()
+
+def get_warm(goose):
+    warmth = get_position("first_morning_light")
+    goose.paddle(to=warmth)
+
+while sun.hits(flower):
+    if len(mosquito.line_of_sight("animals") ) < 1:
+        mosquito.stays(on=flower.petal)
+
+def breakfast(spider_web, bee):
+    if bee.is_hungry():
+        bee.eats(spider_web.insects_caught.pop(0))
+
+def collecting_surplus(spider_web):
+    if spider_web.under_tree:
+        spider_web.collect("yellow tree pollen")
 
 for _ in city.animals:
     counter += 1
     if counter > 5: return False
 
-class City(Sound):
-    def __init__(self):
-        self.construction = True
+def chooses_not_to_fly(crow):
+    if "car" not in crow.line_of_sight:
+        crow.jaywalks()
 
-def looks_down(self):
-    cloud = self
-    cloud.has_seen_cities = True
+while dew in grass:
+    if hour_of_day > 8:
+        some_points_in_grass.sparkle()
 
-def bridge_at_night(car, we):
-    car.hovers_on = "invisible_line"
-    we.go_under = True
+while grass in roof.layers:
+    courtyard.actions.append
+        (butterfly_wings.flap())
 
 while boat in canal:
     boat.boards.creak()
@@ -40,81 +48,13 @@ def trap(water):
     water.flows({ "down":"a human city",
                 "to":"a human sea"})
 
-while sun.is_falling():
-    wall_shadow.grows_taller()
-    picnic.gets_cold()
-
-def earthquake(minute_elapsed):
-    for second in minute_elapsed:
-        building.probability_to_tumble *= 1.01
-
-class CityLandscape:
-    def __init__(self, garden_plot):
-        self.forest = garden_plot
-
-while grass in roof.layers:
-    courtyard.actions.append(
-        butterfly_wings.flaps)
-
-while dew in grass:
-    if hour_of_day > 8:
-        some_points_in_grass.sparkle()
-
-while sun.hits(pedal):
-    if len( mosquito.line_of_sight("animals") ) < 1:
-        mosquito.stays(on=pedal)
-
 def city_transport():
     for jogger in park:
         cars_in_road += 5
 
-def jogged_outside():
-    if today.has_rained and rain.has_stopped:
-        dirt_path.append(puddle)
-
-while trail.is_used:
-    weeds.tear()
-    rocks.erode()
-
-def neighbourhood_walk():
-    while people.walk():
-        bugs.live()
-
-def chooses_not_to_fly(crow):
-    if "car" not in crow.line_of_sight:
-        crow.jaywalks()
-
 for cobblestone in path:
     if cobblestone.has_history:
         return "WW1"
-
-if grass.has_stopped_growing():
-    neighbours.move_out()
-    I.think("how could I saved the ground?")
-
-def rust(building):
-    if building.is_old and building in low_income_areas:
-        deteriorate(building.facade_materials)
-
-for drop in glass_window:
-    if drop.get_no_wind() and weather.is_cold():
-        drop.stays()
-
-def cooling_food(food, window, pigeon):
-    if distance_between(pigeon, window) < 10:
-        window.food_on_ledge.deque()
-
-def estimate_mosquito_position(mosquito, room):
-    if mosquito.is_indoors:
-        return room.objects["organic_trash"].position
-
-def collecting_surplus(spider_web):
-    if spider_web.under_tree:
-        spider_web.collect("tree pollen")
-
-def water_plants(flower):
-    if flower.is_fake():
-        wipe_dust(flower)
 
 def dog_poops():
     soil.feeds()
@@ -124,38 +64,100 @@ def dog_escapes(days_gone):
     if days_gone > 5:
         dog.owner = None
 
+class CityLandscape:
+    def __init__(self, garden_plot):
+        self.forest = garden_plot
+
+class City(Sound):
+    def __init__(self):
+        self.construction = True
+
 for year in tree.life:
     adjacent_sidewalk.bends()
     concrete.breaks()
-
-if rat.eats("poison"):
-    blood.can_clot = True
-    you.clean_houses()
 
 def gardening(plot):
     if plot.has_died:
         plot.color = "#656255"
 
-def regeneration(city):
+def neighbourhood_walk():
+    while people.walk():
+        bugs.live()
+
+def looks_down(self):
+    cloud = self
+    cloud.has_seen_cities = True
+
+def jogged_outside():
+    if today.has_rained and rain.has_stopped:
+        dirt_path.append(puddle)
+
+while trail.is_used:
+    weeds.tear()
+    rocks.erode()
+
+for drop on glass_window:
+    if drop.gets_no_wind() and weather.is_cold():
+        drop.keeps_holding_on()
+
+def water_plants(flower):
+    if flower.is_fake():
+        wipe_dust(flower)
+
+def estimate_mosquito_position(mosquito, room):
+    if mosquito.is_indoors:
+        return room.objects["organic_trash"].position
+
+def cooling_food(food, window, pigeon):
+    if distance_between(pigeon, window) < 10:
+        window.food_on_ledge.deque()
+
+if rat.eats("poison"):
+    blood.can_clot = True
+    you.sweep_floor()
+
+if grass.has_stopped_growing():
+    neighbours.move_out()
+    I.think("how could I have saved the ground?")
+
+def rust(building):
+    if building.is_old and building in low_income_areas:
+        deteriorate(building.facade_materials)
+
+def start_crisis(world):
+    while world.cities.get_built():
+        world.skies.disappear()
+
+def earthquake(minute_elapsed):
+    for second in minute_elapsed:
+        building.probability_to_tumble *= 1.01
+
+def degeneration(city):
     for pond in city:
         print("a shiny wrapper floats")
 
-def autumn_starts_early(global_warming):
-    if orange_leaves.have_fallen():
+def seasonal_mismatch(global_warming):
+    if autumn_leaves.have_fallen() and time_to_autumn > 40: #days
         global_warming = True
 
 while park.unkept():
-    vines.grow()
-    bees.coalesce()
+     wisteria.grow()
+         bees.coalesce()
+
+while sun.is_falling():
+    wall_shadow.grows_taller()
+    picnic.gets_cold()
 
 def night_haul(spider_web,firefly):
     if spider_web not in firefly.line_of_sight():
         spider_web.enqueue(firefly)
 
-def get_warm(goose):
-    warmth = get_position("first_morning_light")
-    goose.paddle_to(warmth)
+def bridge_at_night(car, we):
+    car.hovers_on = "invisible_line"
+    we.go_under = True
 
-def breakfast(spider_web, bee):
-    if bee.is_hungry():
-        bee.eats(spider_web.insects_caught.pop(0))
+def drive(bus):
+    if bus.speeds({"at":"night"}):
+        corn.drifts(by = 2) #in inches
+
+
